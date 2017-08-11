6888cb043fe26065228b6a90ac8704dacc869ad3
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-11 18:34:09 +0200

    Use more slick OLS Namespace builder

--- src/bio2bel_uberon/cli.py
+++ src/bio2bel_uberon/cli.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""Run this script with either :code:`python3 -m bio2bel_uberon arty`"""
-
 from __future__ import print_function
 
 import logging
@@ -10,28 +8,34 @@ import sys
 import click
 
 from ols_client import OLS_BASE
-from .run import deploy_to_arty, write_belns
+from pybel_tools.ols_utils import OlsNamespaceOntology
+from .run import MODULE_FUNCTIONS, MODULE_DOMAIN, MODULE_NAME
 
 
 @click.group()
-def main():
-    """Output gene family hierarchy as BEL script and BEL namespace"""
+@click.option('-b', '--ols-base', help="OLS base url. Defaults to {}".format(OLS_BASE))
+@click.pass_context
+def main(ctx, ols_base):
+    """Utilities for UBERON BEL namespace generation"""
     logging.basicConfig(level=10, format="%(asctime)s - %(levelname)s - %(message)s")
+    ctx.obj = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS, ols_base=ols_base)
 
 
 @main.command()
 @click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
-@click.option('-b', '--ols-base', help="OLS base url. Defaults to {}".format(OLS_BASE))
-def write(output, ols_base):
-    """Writes BEL namespace to standard out"""
-    write_belns(output, ols_base=ols_base)
+@click.pass_context
+def write(ctx, output):
+    """Writes BEL namespace"""
+    ctx.obj.write(output)
 
 
 @main.command()
-@click.option('-b', '--ols-base', help="OLS base url. Defaults to {}".format(OLS_BASE))
-def arty(ols_base):
-    """Deploy to artifactory"""
-    deploy_to_arty(ols_base=ols_base)
+@click.option('--no-hash-check', is_flag=True)
+@click.pass_context
+def deploy(ctx, no_hash_check):
+    """Deploy to Artifactory"""
+    success = ctx.obj.deploy(hash_check=(not no_hash_check))
+    click.echo('Deployed to {}'.format(success) if success else 'Duplicate not deployed')
 
 
 if __name__ == '__main__':
--- src/bio2bel_uberon/run.py
+++ src/bio2bel_uberon/run.py
@@ -1,59 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from ols_client import get_labels, get_metadata
 from pybel.constants import NAMESPACE_DOMAIN_OTHER
-from pybel_tools.definition_utils import write_namespace
-from pybel_tools.resources import deploy_namespace, get_today_arty_namespace
+from pybel_tools.ols_utils import OlsNamespaceOntology
 
 __all__ = [
     'MODULE_NAME',
+    'MODULE_DOMAIN',
+    'MODULE_FUNCTIONS',
     'write_belns',
     'deploy_to_arty',
 ]
 
 MODULE_NAME = 'uberon'
+MODULE_DOMAIN = NAMESPACE_DOMAIN_OTHER
+MODULE_FUNCTIONS = 'A'
 
+metadata = {
+    'ontology_name': MODULE_NAME,
+    'namespace_domain': MODULE_DOMAIN,
+    'functions': MODULE_FUNCTIONS,
+}
 
-def write_belns(file, ols_base=None):
-    """Serializes UBERON to a BEL namespace.
+ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS)
 
-    :param file file: A write-enable file or file-like
-    :param str ols_base: An optional, custom OLS base url
-    """
-    metadata = get_metadata(MODULE_NAME, ols_base=ols_base)
-    values = get_labels(MODULE_NAME, ols_base=ols_base)
-
-    config = metadata['config']
-
-    write_namespace(
-        namespace_name=config['title'],
-        namespace_keyword=config['preferredPrefix'],
-        namespace_domain=NAMESPACE_DOMAIN_OTHER,
-        namespace_description=config['description'],
-        namespace_version=config['version'],
-        citation_name=config['title'],
-        citation_url=config['versionIri'],
-        author_name='Charles Tapley Hoyt',
-        author_contact='charles.hoyt@scai.fraunhofer.de',
-        author_copyright='Creative Commons by 4.0',
-        values=values,
-        functions='A',
-        file=file
-    )
-
-
-def deploy_to_arty(ols_base=None):
-    """Gets the data and writes BEL namespace file to Artifactory
-
-    :param str ols_base: An optional, custom OLS base url
-    """
-    file_name = get_today_arty_namespace(MODULE_NAME)
-
-    with open(file_name, 'w') as file:
-        write_belns(file, ols_base=ols_base)
-
-    deploy_namespace(file_name, MODULE_NAME)
-
-
-if __name__ == '__main__':
-    deploy_to_arty()
+write_belns = ontology.write
+deploy_to_arty = ontology.deploy
