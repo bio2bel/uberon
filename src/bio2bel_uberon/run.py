53951428b8e0806cba003f597c40f63649b13303
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-18 22:06:57 +0100

    Update variable enclosure

--- src/bio2bel_uberon/run.py
+++ src/bio2bel_uberon/run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pybel.constants import NAMESPACE_DOMAIN_OTHER, ABUNDANCE
+from pybel.constants import ABUNDANCE, NAMESPACE_DOMAIN_OTHER
 from pybel_tools.ols_utils import OlsNamespaceOntology
 
 __all__ = [
@@ -15,7 +15,12 @@ MODULE_NAME = 'uberon'
 MODULE_DOMAIN = NAMESPACE_DOMAIN_OTHER
 MODULE_FUNCTION = ABUNDANCE
 
-ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTION)
 
-write_belns = ontology.write_namespace
-deploy_to_arty = ontology.deploy_namespace
+def write_belns(file):
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTION)
+    return ontology.write_namespace(file)
+
+
+def deploy_to_arty():
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTION)
+    return ontology.deploy_namespace()
